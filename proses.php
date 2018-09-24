1c7efcbc5d4ba3aa8c264526a8fea611e8aab3bf
melishad <42725372+melishad@users.noreply.github.com>
2018-09-24 10:54:26 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,3 +1,24 @@
 <?php
+$data = $_FILES['gambar']['name'];
+if (isset($_POST['submit'])) {
+	move_uploaded_file($_FILES['gambar']['tmp_name'], "upload/".$_FILES['gambar']['name']);
+	echo "<h1>Gambar Anda Berhasil Diunggah</h1>";
+	echo "<br>";
+	//echo "<img src = 'upload/$data' >";
+}
+
+$hobi = $_POST['hobi'];
+echo "<h1>Hobi yang Anda pilih :</h1>";
+foreach ($hobi as $hb) {
+echo "- $hb  <br>";
+
+}
+echo "<img src = 'upload/$data' >";
+
 
 ?>
+
+
+<form action="halaman akhir.php">
+	<input type="submit" name="hapus" value="hapus">
+</form>
